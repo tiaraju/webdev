62b10aac88b72ee5abdf69bb4a6f0328da00ecbb
Tiaraju Smaneoto <tiaraju.smaneoto@ccc.ufcg.edu.br>
2016-03-04 17:23:56 -0300

    first task

---
+++ exercicio1/js/librarycontroller.js
@@ -0,0 +1,8 @@
+var library= angular.module('library',[]);
+
+library.controller('libraryController',function($scope){
+	var self = this;
+
+	self.books=collection;
+
+});
---
+++ exercicio1/model/library.js
@@ -0,0 +1,71 @@
+var collection = [
+{
+	"title":"asdasd " ,
+	"writers":[],
+	"description":" ",
+	"picUrl":" ",
+	"price":0
+},
+{
+	"title":"lkjfghfd " ,
+	"writers":[],
+	"description":" ",
+	"picUrl":" ",
+	"price":0
+},
+{
+	"title":" " ,
+	"writers":[],
+	"description":" ",
+	"picUrl":" ",
+	"price":0
+},
+{
+	"title":" " ,
+	"writers":[],
+	"description":" ",
+	"picUrl":" ",
+	"price":0
+},
+{
+	"title":" " ,
+	"writers":[],
+	"description":" ",
+	"picUrl":" ",
+	"price":0
+},
+{
+	"title":" " ,
+	"writers":[],
+	"description":" ",
+	"picUrl":" ",
+	"price":0
+},
+{
+	"title":" " ,
+	"writers":[],
+	"description":" ",
+	"picUrl":" ",
+	"price":0
+},
+{
+	"title":" " ,
+	"writers":[],
+	"description":" ",
+	"picUrl":" ",
+	"price":0
+},
+{
+	"title":" " ,
+	"writers":[],
+	"description":" ",
+	"picUrl":" ",
+	"price":0
+},
+{
+	"title":" " ,
+	"writers":[],
+	"description":" ",
+	"picUrl":" ",
+	"price":0
+}];
