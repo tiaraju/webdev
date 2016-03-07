c35991b1eb929def934907ad67c0a1ad4f26952b
tiaraju <tiarajums@gmail.com>
2016-03-07 01:09:09 -0300

    adding methods

--- exercicio1/js/librarycontroller.js
+++ exercicio1/js/librarycontroller.js
@@ -1,8 +1,54 @@
-var library= angular.module('library',[]);
+(function () {
+	var library= angular.module('library',['ngMaterial']);
+	library.controller('libraryController',function($scope,$mdDialog){
+		var self = this;
 
-library.controller('libraryController',function($scope){
-	var self = this;
+		self.books=collection;
+		self.book = {};
+		self.comment;
+		$scope.bookID;
 
-	self.books=collection;
+		
 
-});
+		$scope.showDialog= function() {
+	       var parentEl = angular.element(document.body);
+	       $mdDialog.show({
+	       	scope:$scope,
+			parent: parentEl,
+			preserveScope:true,
+			clickOutsideToClose: true,
+			templateUrl:'view/modal.html',
+	      });
+	    };
+
+	    $scope.showCommentDialog= function() {
+	       var parentEl = angular.element(document.body);
+	       $mdDialog.show({
+	       	scope:$scope,
+			parent: parentEl,
+			preserveScope:true,
+			clickOutsideToClose: true,
+			templateUrl:'view/commentModal.html',
+	      });
+	    };
+
+
+
+	    self.closeDialog = function() {
+	    	console.log(self.comment);
+	    	console.log($scope.bookID);
+	    	if(Object.keys(self.book).length != 0){
+		    	self.books.push(self.book);
+	    	}else{
+	    		//self.books[self.bookId]
+	    	}
+
+	    	$mdDialog.hide();
+
+	    };
+
+
+	
+
+	});
+})();
--- exercicio1/model/library.js
+++ exercicio1/model/library.js
@@ -1,71 +1,91 @@
 var collection = [
 {
-	"title":"asdasd " ,
+	"id":0,
+  	"title":"asdasd " ,
 	"writers":[],
 	"description":" ",
 	"picUrl":" ",
-	"price":0
+	"price":0,
+	"comments":[]
 },
 {
+	"id":1,
 	"title":"lkjfghfd " ,
 	"writers":[],
 	"description":" ",
 	"picUrl":" ",
-	"price":0
+	"price":0,
+	"comments":[]
 },
 {
-	"title":" " ,
+	"id":2,
+  	"title":" " ,
 	"writers":[],
 	"description":" ",
 	"picUrl":" ",
-	"price":0
+	"price":0,
+	"comments":[]
 },
 {
-	"title":" " ,
+	"id":3,
+  	"title":" " ,
 	"writers":[],
 	"description":" ",
 	"picUrl":" ",
-	"price":0
+	"price":0,
+	"comments":[]
 },
 {
-	"title":" " ,
+	"id":4,
+  	"title":" " ,
 	"writers":[],
 	"description":" ",
 	"picUrl":" ",
-	"price":0
+	"price":0,
+	"comments":[]
 },
 {
-	"title":" " ,
+	"id":5,
+  	"title":" " ,
 	"writers":[],
 	"description":" ",
 	"picUrl":" ",
-	"price":0
+	"price":0,
+	"comments":[]
 },
 {
-	"title":" " ,
+	"id":6,
+  	"title":" " ,
 	"writers":[],
 	"description":" ",
 	"picUrl":" ",
-	"price":0
+	"price":0,
+	"comments":[]
 },
 {
-	"title":" " ,
+	"id":7,
+  	"title":" " ,
 	"writers":[],
 	"description":" ",
 	"picUrl":" ",
-	"price":0
+	"price":0,
+	"comments":[]
 },
 {
-	"title":" " ,
+	"id":8,
+  	"title":" " ,
 	"writers":[],
 	"description":" ",
 	"picUrl":" ",
-	"price":0
+	"price":0,
+	"comments":[]
 },
 {
-	"title":" " ,
+	"id":9,
+  	"title":" " ,
 	"writers":[],
 	"description":" ",
 	"picUrl":" ",
-	"price":0
+	"price":0,
+	"comments":[]
 }];
