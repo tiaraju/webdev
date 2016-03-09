625c23ab30808ea2b1edb8ca8bea56b47a7c67f8
tiaraju <tiarajums@gmail.com>
2016-03-09 01:57:57 -0300

    port

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   return res.sendFile(path.join(__dirname+'/../public/index.html'));
 });
 
-app.listen(5000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
