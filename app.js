700794be755f7ebb7eb723add7bf8798a7adbfbd
tiaraju <tiarajums@gmail.com>
2016-03-09 01:52:07 -0300

    setting route

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var express = require('express'),
-    app = express();
+var express = require('express');
+var app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  return res.sendFile(path.join(__dirname+'/../public/index.html'));
+});
 
 app.listen(5000);
