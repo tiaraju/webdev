1a23bb765354feacffd2d54250ff970815eeef79
tiaraju <tiarajums@gmail.com>
2016-03-23 16:27:01 -0300

    changing project structure

---
+++ js/librarycontroller.js
@@ -0,0 +1,97 @@
+(function () {
+	var library= angular.module('library',['ngAnimate', 'ui.bootstrap']);
+	library.controller('libraryController',function($scope, $uibModal,$http){
+		var self = this;
+		var URL = "http://localhost:3000/";
+
+		
+		$scope.getBooks = function(){
+			console.log("chamou");
+			$http.get(URL+"books")
+				.then(function(result) {
+		        	self.books = result.data;
+		    	},function (response) {
+		    		console.log(response);
+	    	});
+		};
+
+		$scope.getBooks();
+		
+		$scope.comment;
+		$scope.book = {}
+		$scope.bookID;
+
+	    $scope.setBookId=function(id) {
+	    	 $scope.bookID = id;
+	    };
+
+	    $scope.showModal = function(){
+		    $scope.modalInstance = $uibModal.open({
+		      animation: $scope.animationsEnabled,
+		      templateUrl: '/views/modal.html',
+		      controller: 'libraryController',
+			  preserveScope:true,
+		      clickOutsideToClose: true,
+		      scope:$scope
+		    });
+		    
+		 };
+
+		$scope.toggleAnimation = function () {
+    		$scope.animationsEnabled = !$scope.animationsEnabled;
+  		};
+
+  		$scope.addBook = function (newbook) {
+  			$scope.book = newbook;
+  			$http.post(URL+"books",newbook).then(function (data,status) {
+    			 $scope.modalInstance.close(); 
+  			}).then(function (data,status){
+    			 $scope.modalInstance.close();
+  			});
+  		};
+
+  		$scope.showCommentDialog = function(){
+  			 $scope.modalInstance = $uibModal.open({
+		      animation: $scope.animationsEnabled,
+		      templateUrl: '/views/commentModal.html',
+		      controller: 'libraryController',
+			  preserveScope:true,
+		      clickOutsideToClose: true,
+		      scope:$scope
+		    });
+
+  		};
+
+		$scope.showEditDialog = function(){
+			$scope.modalInstance = $uibModal.open({
+		      animation: $scope.animationsEnabled,
+		      templateUrl: '../view/editModal.html',
+		      controller: 'libraryController',
+			  preserveScope:true,
+		      clickOutsideToClose: true,
+		      scope:$scope
+		    });
+
+  		};
+
+  		$scope.saveComment = function(comentario){
+  			self.books[$scope.bookID].comments.push(comentario);
+			$scope.modalInstance.close();
+			$scope.getBooks();
+  		};
+
+  		$scope.saveBook = function(book){
+  			self.books[$scope.bookID]=book;
+			$scope.modalInstance.close();
+			$scope.getBooks();
+
+  		};
+
+  		$scope.cancel = function () {
+    		$scope.modalInstance.dismiss('cancel');
+			$scope.getBooks();
+
+  		};
+
+	});
+})();
---
+++ routes/books.js
@@ -0,0 +1,117 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+	res.send(collection);
+});
+
+router.post('/',function (req,res) {
+	 var book = req.body;
+	 collection.push(book);
+	 res.status(200);
+	 res.send({});
+});
+
+router.put('/:bookId',function (req,res) {
+	 var book = req.body;
+	 var bookId = req.params.bookId;
+	 collection[bookId]=book;
+});
+
+
+
+
+var collection = [
+{
+	"id":0,
+  	"title":"dom casmurro " ,
+	"writers":["jose"],
+	"description":" ",
+	"picUrl":" ",
+	"price":40,
+	"comments":[]
+},
+{
+	"id":1,
+	"title":"o cortiço" ,
+	"writers":["jose"],
+	"description":" ",
+	"picUrl":" ",
+	"price":33,
+	"comments":[]
+},
+{
+	"id":2,
+  	"title":"memoria postumas " ,
+	"writers":["jose"],
+	"description":" ",
+	"picUrl":" ",
+	"price":12,
+	"comments":[]
+},
+{
+	"id":3,
+  	"title":" iracema" ,
+	"writers":["jose"],
+	"description":" ",
+	"picUrl":" ",
+	"price":10,
+	"comments":[]
+},
+{
+	"id":4,
+  	"title":" biografia " ,
+	"writers":["jose","joao"],
+	"description":" ",
+	"picUrl":" ",
+	"price":10,
+	"comments":[]
+},
+{
+	"id":5,
+  	"title":"auto ajuda " ,
+	"writers":["jose","joao"],
+	"description":" ",
+	"picUrl":" ",
+	"price":20,
+	"comments":[]
+},
+{
+	"id":6,
+  	"title":"historia" ,
+	"writers":["jose","joao"],
+	"description":" ",
+	"picUrl":" ",
+	"price":10,
+	"comments":[]
+},
+{
+	"id":7,
+  	"title":"matematica " ,
+	"writers":["jose","joao"],
+	"description":" ",
+	"picUrl":" ",
+	"price":10,
+	"comments":[]
+},
+{
+	"id":8,
+  	"title":" python" ,
+	"writers":["jose","joao"],
+	"description":" ",
+	"picUrl":" ",
+	"price":10,
+	"comments":[]
+},
+{
+	"id":9,
+  	"title":" javascript" ,
+	"writers":["jose","joao"],
+	"description":" ",
+	"picUrl":" ",
+	"price":22,
+	"comments":[]
+}];
+
+module.exports = router;
